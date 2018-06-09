a61096a62d98f2f878a3a63df3f8ffa160599559
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-10 07:35:03 +0900

    Added Microsoft.EntityFrameworkCore.Tools nuget package.

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -6,6 +6,13 @@ namespace SamuraiApp.Data
     // Microsoft.EntityFrameworkCore.SqlServer
     // https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.SqlServer/
     // Install-Package -Id Microsoft.EntityFrameworkCore.SqlServer -ProjectName SamuraiApp.Data
+    //
+    // To create migration file, it requires Microsoft.EntityFrameworkCore.Tools package.
+    // Run the "get-help entityframeworkcore" command in the Package Manager Console to get
+    // a help message how to create migration file.
+    // Microsoft.EntityFrameworkCore.Tools
+    // https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.Tools/
+    // Install-Package -Id Microsoft.EntityFrameworkCore.Tools -ProjectName SamuraiApp.Data
     public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
