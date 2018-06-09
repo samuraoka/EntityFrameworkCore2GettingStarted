e293bd00a9b41d9bf7fecf03160f2d9f8c70e2e0
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-10 01:55:59 +0900

    Added models.

---
+++ SamuraiApp/SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        public int BattleId { get; set; }
+
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+    }
+}
